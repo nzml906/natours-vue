5d111a3d08756a5dad22675928afa1bf3d887ab2
Nazmul <grey000906@gmail.com>
2020-07-17 03:19:19 +0600

    deploy portfolio

---
+++ precache-manifest.e7b4718455f4c7226cf7d56fae46fd26.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac49d54c66aec68c77e",
+    "url": "/natours-vue/css/app.7dd96ae2.css"
+  },
+  {
+    "revision": "86602809f305e0d26b2ca5318437b1db",
+    "url": "/natours-vue/img/favicon.png"
+  },
+  {
+    "revision": "cd69d3552824dc8e99c3f2953de2735b",
+    "url": "/natours-vue/img/hero-small.cd69d355.jpg"
+  },
+  {
+    "revision": "cd69d3552824dc8e99c3f2953de2735b",
+    "url": "/natours-vue/img/hero-small.jpg"
+  },
+  {
+    "revision": "6e09f4102b0d64adc38d21bd9c89a9a7",
+    "url": "/natours-vue/img/hero.6e09f410.jpg"
+  },
+  {
+    "revision": "6e09f4102b0d64adc38d21bd9c89a9a7",
+    "url": "/natours-vue/img/hero.jpg"
+  },
+  {
+    "revision": "4c42c11f1fc0203483c21f2d6eeb8896",
+    "url": "/natours-vue/img/logo-green-1x.png"
+  },
+  {
+    "revision": "3365abba6477c48e6be892cf6faa8f25",
+    "url": "/natours-vue/img/logo-green-2x.png"
+  },
+  {
+    "revision": "c98cd0ce9ab26a0c7e6253f613ce21ac",
+    "url": "/natours-vue/img/logo-green-small-1x.png"
+  },
+  {
+    "revision": "888043d544c9ed77285afbc360ffd27e",
+    "url": "/natours-vue/img/logo-green-small-2x.png"
+  },
+  {
+    "revision": "40d4ef82deebd0521e91006d961ead40",
+    "url": "/natours-vue/img/logo-white.png"
+  },
+  {
+    "revision": "7fdf2a886516e020e300ae6bd544ab7d",
+    "url": "/natours-vue/img/nat-1-large.jpg"
+  },
+  {
+    "revision": "d6c15bf0efcaf4c03c5cb0c9078cdd17",
+    "url": "/natours-vue/img/nat-1.jpg"
+  },
+  {
+    "revision": "2797561ff2a3de15c850f2744b73b6d1",
+    "url": "/natours-vue/img/nat-10.2797561f.jpg"
+  },
+  {
+    "revision": "2797561ff2a3de15c850f2744b73b6d1",
+    "url": "/natours-vue/img/nat-10.jpg"
+  },
+  {
+    "revision": "7a25f1999a15760508f5e6131245aeac",
+    "url": "/natours-vue/img/nat-2-large.jpg"
+  },
+  {
+    "revision": "4d3783688636b59662283e19991a2912",
+    "url": "/natours-vue/img/nat-2.jpg"
+  },
+  {
+    "revision": "417d3d741a7a47b3d6ef0307228033a0",
+    "url": "/natours-vue/img/nat-3-large.jpg"
+  },
+  {
+    "revision": "cc2b05433cbad0b369d131d02c0cac6a",
+    "url": "/natours-vue/img/nat-3.jpg"
+  },
+  {
+    "revision": "61bbf12ae90a30cd432d77087921a02f",
+    "url": "/natours-vue/img/nat-4.61bbf12a.jpg"
+  },
+  {
+    "revision": "61bbf12ae90a30cd432d77087921a02f",
+    "url": "/natours-vue/img/nat-4.jpg"
+  },
+  {
+    "revision": "19f9a22f45a15fbf80d420862bcadffb",
+    "url": "/natours-vue/img/nat-5.19f9a22f.jpg"
+  },
+  {
+    "revision": "19f9a22f45a15fbf80d420862bcadffb",
+    "url": "/natours-vue/img/nat-5.jpg"
+  },
+  {
+    "revision": "1ef6d764fc819e080d40aa72dfe03aa6",
+    "url": "/natours-vue/img/nat-6.1ef6d764.jpg"
+  },
+  {
+    "revision": "1ef6d764fc819e080d40aa72dfe03aa6",
+    "url": "/natours-vue/img/nat-6.jpg"
+  },
+  {
+    "revision": "5f846d16e6be9a73e6eb73f620379af2",
+    "url": "/natours-vue/img/nat-7.5f846d16.jpg"
+  },
+  {
+    "revision": "5f846d16e6be9a73e6eb73f620379af2",
+    "url": "/natours-vue/img/nat-7.jpg"
+  },
+  {
+    "revision": "14efd82b81787a9764b2cb7eb7afd3e8",
+    "url": "/natours-vue/img/nat-8.jpg"
+  },
+  {
+    "revision": "b525a99bcf20d08b2aaf6dd83b7be623",
+    "url": "/natours-vue/img/nat-9.jpg"
+  },
+  {
+    "revision": "ffcd876c5561db066c09f5922b384473",
+    "url": "/natours-vue/img/video.mp4"
+  },
+  {
+    "revision": "f556a2c3f37c228f95a5eac3fae8c4a7",
+    "url": "/natours-vue/img/video.webm"
+  },
+  {
+    "revision": "0b70405c99547d12c3a7400046beafe7",
+    "url": "/natours-vue/index.html"
+  },
+  {
+    "revision": "9ac49d54c66aec68c77e",
+    "url": "/natours-vue/js/app.c1ba9891.js"
+  },
+  {
+    "revision": "9e83726d6b1a868d032f",
+    "url": "/natours-vue/js/chunk-vendors.bdc9172a.js"
+  },
+  {
+    "revision": "b0167e0313cc0025fda7b6d725ba0d1f",
+    "url": "/natours-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/natours-vue/robots.txt"
+  }
+]);
